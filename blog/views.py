6ba8c4161784bdf343e3a960a7b5933ec8ed6a34
chris.mcavoy <chris.mcavoy@645c6a4a-b42a-0410-a30c-7168d278c38c>
2007-03-04 19:52:11 +0000

    moved templates over from tastebudchicago.com and started editing them

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,8 @@ from django.template import Context, loader
 import settings
 app = __import__("%s" % settings.MODULE_NAME,globals(),locals(),['settings'])
 # app settings become app.settings.whatever
-blog = __import__("%s.blog.models" % settings.MODULE_NAME,globals(),locals(),['models'])
-# blog models become blog.models.BlogEntry etc etc
+blog = __import__("%s.blog.models" % settings.MODULE_NAME,globals(),locals(),['*'])
+# blog models become blog.BlogEntry etc etc
 
 from datetime import datetime as d
 
@@ -13,8 +13,9 @@ def standard_context():
     c = Context({'site_name':app.settings.SITE_NAME,'feed_url':app.settings.FEED_URL})
     return c
     
-def index(request):
-    t = loader.get_template("index.html")
+def blog_entries(request):
+    t = loader.get_template("blog.html")
     c = standard_context()
+    c['entries'] = blog.BlogEntry.objects.all()
     return HttpResponse(t.render(c))
 
