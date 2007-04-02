b684159c2682e9f383f5dde273e38f9c37e7f546
chris.mcavoy <chris.mcavoy@645c6a4a-b42a-0410-a30c-7168d278c38c>
2007-04-02 02:15:25 +0000

    changed podcasts to use a filename as an attribute rather than a data upload...http uploads for biggish podcasts suck

--- blog/models.py
+++ blog/models.py
@@ -58,13 +58,13 @@ class Photo(models.Model):
         ordering = ['caption']
         
 class Podcast(models.Model):
-    data = models.FileField(upload_to='podcasts')
+    filename = models.CharField(maxlength=255)
     title = models.CharField(maxlength=255)
     description = models.TextField(blank=True, null=True)
 
     def get_absolute_url(self):
         site = Site.objects.get_current().domain
-        return "http://%s/%s" % (site,self.get_data_url())
+        return "http://%s/%s/%s" % (site,settings.PODCAST_BASE,self.filename)
     
     def link(self):
         return "<a href=\"%s\">%s</a>" % (self.get_absolute_url(), self.title)
