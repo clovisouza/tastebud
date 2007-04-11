00f6749d4693934f8c6d9d587ab61d327493d095
chris.mcavoy <chris.mcavoy@645c6a4a-b42a-0410-a30c-7168d278c38c>
2007-04-11 18:17:11 +0000

    added pubdate to the rss feed

--- trunk/blog/rss.py
+++ trunk/blog/rss.py
@@ -10,3 +10,17 @@ class BlogFeed(Feed):
 
     def items(self):
         return blog.BlogEntry.objects.filter(date_added__lte=d.now()).order_by('-date_added')[:10]
+    
+    def item_author_name(self, item):
+        """This doesn't seem to work"""
+        #r = ""
+        #for author in item.authors:
+        #    r = "%s %s " % (r,author)
+        #return r
+        return item.authors
+    
+    def item_link(self, item):
+        return item.get_absolute_url()
+    
+    def item_pubdate(self, item):
+        return item.date_added
